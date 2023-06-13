fb2a11f6e7c763dd272badf32aa91d6bf4bc4f84
ahmadSoliman94 <ahmad.suliman94@hotmail.com>
2023-06-13 23:34:27 +0200

    Yolo8

--- predict.py
+++ predict.py
@@ -11,13 +11,15 @@ cap=cv2.VideoCapture('./video.mp4')
 
 
 
-
+#  loop over frames from the video file stream
 while (cap. isOpened()):
     ret, frame= cap.read()
     if ret:
-        res=model.predict(frame,conf=0.3)
+
+        # detect objects in the input frame 
+        res=model.predict(frame,conf=0.3) # conf is confidence threshold 
         res_plotted=res[0].plot()
-        cv2. imshow('frame', res_plotted)
+        cv2.imshow('frame', res_plotted)
         if cv2.waitKey(20)& 0xFF== ord ('q'):
             break
 cap. release()
