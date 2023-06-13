3210bbb2faf606a9336e04d63c6ebad10522fc0e
ahmadSoliman94 <ahmad.suliman94@hotmail.com>
2023-06-13 11:59:42 +0200

    download images

---
+++ download_images.py
@@ -0,0 +1,8 @@
+from simple_image_download import simple_image_download as simp
+
+# Instantiate the class
+response = simp.simple_image_download
+
+
+# Download images to the specified directory
+response().download('./Red traffic light', 500)
